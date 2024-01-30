aa94a9dec340d86528ea7c0a94d0b462238428e8
sefat006 <sefatur76@gmail.com>
2024-01-30 23:46:38 +0600

    api add done

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react"
+
+const App=()=> {
+
+  let APIData = useRef(null);
+  let myPTag = useRef();
+  
+  const fetchData = async () =>{
+    const res = await fetch(`https://dummyjson.com/products/1`);
+     APIData.current = await res.json();    
+  }
+
+  const showData = () =>{
+    myPTag.current.innerText = JSON.stringify(APIData.current);
+  }
+
+  return (
+    <div>
+      <button onClick={fetchData}>Call API</button>
+      <button onClick={showData} >Show Data</button>
+      <p ref={myPTag}></p>
+    </div>
+  )
+}
+
+export default App;
+
+// 1st button click e data fetch korbo
+// tarpor APIData te data store korbo
+// tarpor APIData theke data show korbo <p> er moddhe
